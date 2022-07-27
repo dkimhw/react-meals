d10f82ab7432f337d71e9c757f3e2b3037811a03
David Kim <dkhw90@gmail.com>
2022-07-27 19:22:30 +0200

    Add state handler for showing and hiding cart modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -5,7 +5,7 @@ import classes from './Modal.module.css';
 
 const Backdrop = (props) => {
   return (
-    <div className={classes.backdrop}></div>
+    <div className={classes.backdrop} onClick={props.onClose}></div>
   );
 };
 
@@ -22,7 +22,7 @@ const portalElement = document.getElementById('overlays');
 const Modal = (props) => {
   return (
     <React.Fragment>
-      {ReactDom.createPortal(<Backdrop />, portalElement) }
+      {ReactDom.createPortal(<Backdrop onClose={props.onClose}/>, portalElement) }
       {ReactDom.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
     </React.Fragment>
   );
